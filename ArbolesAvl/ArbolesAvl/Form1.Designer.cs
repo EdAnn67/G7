6ebf1d5920c320c0b86664b86cadb036c9ae17e4
unknown <edann67@gmail.com>
2021-03-17 22:50:19 -0600

    Guia7

---
+++ ArbolesAvl/ArbolesAvl/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace ArbolesAvl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.valor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.errores = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.post = new System.Windows.Forms.RadioButton();
+            this.ino = new System.Windows.Forms.RadioButton();
+            this.pre = new System.Windows.Forms.RadioButton();
+            this.Orden = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errores)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(667, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Altura del arbol:";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(753, 20);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(13, 13);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnSalir);
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.btnIngresar);
+            this.panel1.Controls.Add(this.valor);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(633, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(155, 272);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(39, 235);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(39, 187);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(39, 142);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(39, 102);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 2;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // valor
+            // 
+            this.valor.Location = new System.Drawing.Point(29, 59);
+            this.valor.Name = "valor";
+            this.valor.Size = new System.Drawing.Size(100, 20);
+            this.valor.TabIndex = 1;
+            this.valor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.valor_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Opciones";
+            // 
+            // errores
+            // 
+            this.errores.ContainerControl = this;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.Orden);
+            this.panel2.Controls.Add(this.post);
+            this.panel2.Controls.Add(this.ino);
+            this.panel2.Controls.Add(this.pre);
+            this.panel2.Location = new System.Drawing.Point(137, 371);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(454, 67);
+            this.panel2.TabIndex = 3;
+            // 
+            // post
+            // 
+            this.post.AutoSize = true;
+            this.post.Location = new System.Drawing.Point(207, 8);
+            this.post.Name = "post";
+            this.post.Size = new System.Drawing.Size(75, 17);
+            this.post.TabIndex = 2;
+            this.post.TabStop = true;
+            this.post.Text = "PostOrden";
+            this.post.UseVisualStyleBackColor = true;
+            // 
+            // ino
+            // 
+            this.ino.AutoSize = true;
+            this.ino.Location = new System.Drawing.Point(121, 8);
+            this.ino.Name = "ino";
+            this.ino.Size = new System.Drawing.Size(67, 17);
+            this.ino.TabIndex = 1;
+            this.ino.TabStop = true;
+            this.ino.Text = "EnOrden";
+            this.ino.UseVisualStyleBackColor = true;
+            // 
+            // pre
+            // 
+            this.pre.AutoSize = true;
+            this.pre.Location = new System.Drawing.Point(26, 8);
+            this.pre.Name = "pre";
+            this.pre.Size = new System.Drawing.Size(70, 17);
+            this.pre.TabIndex = 0;
+            this.pre.TabStop = true;
+            this.pre.Text = "PreOrden";
+            this.pre.UseVisualStyleBackColor = true;
+            // 
+            // Orden
+            // 
+            this.Orden.AutoSize = true;
+            this.Orden.Location = new System.Drawing.Point(26, 47);
+            this.Orden.Name = "Orden";
+            this.Orden.Size = new System.Drawing.Size(0, 13);
+            this.Orden.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Solicitar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errores)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox valor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ErrorProvider errores;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton post;
+        private System.Windows.Forms.RadioButton ino;
+        private System.Windows.Forms.RadioButton pre;
+        private System.Windows.Forms.Label Orden;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ArbolesAvl/ArbolesAvl/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArbolesAvl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int cont = 0;
+        int dato = 0;
+        int datb = 0;
+        int cont2 = 0;
+        DibujaAVL arbolAVL = new DibujaAVL(null);
+        DibujaAVL arbolAVL_Letra = new DibujaAVL(null);
+        Graphics g;
+        int pintaR = 0;
+
+        private void Form1_Paint(object sender, PaintEventArgs en)
+        {
+            en.Graphics.Clear(this.BackColor);
+            en.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            en.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g = en.Graphics;
+            arbolAVL.DibujarArbol(g, this.Font,
+            Brushes.White, Brushes.Black, Pens.White, datb, Brushes.Black);
+            datb = 0;
+            if (pintaR == 1)
+            {
+                arbolAVL.colorear(g, this.Font, Brushes.Black, Brushes.Yellow, Pens.Blue, arbolAVL.Raiz, post.Checked, ino.Checked, pre.Checked);
+                pintaR = 0;
+            }
+            if (pintaR == 2)
+            {
+                arbolAVL.colorearB(g, this.Font, Brushes.White, Brushes.Red, Pens.White, arbolAVL.Raiz,
+int.Parse(valor.Text));
+                pintaR = 0;
+
+            }
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            errores.Clear();
+            if (valor.Text == "")
+            {
+                errores.SetError(valor, "Valor obligatorio");
+            }
+            else
+            {
+                try
+                {
+                    dato = int.Parse(valor.Text);
+                    arbolAVL.Insertar(dato);
+                    valor.Clear();
+                    valor.Focus();
+                    lblAltura.Text = arbolAVL.Raiz.getAltura(arbolAVL.Raiz).ToString();
+                    cont++;
+                    Refresh();
+                    Refresh();
+                }
+                catch (Exception ex)
+                {
+                    errores.SetError(valor, "Debe ser numérico");
+                }
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            errores.Clear();
+            if (valor.Text == "")
+            {
+                errores.SetError(valor, "Valor obligatorio");
+            }
+            else
+            {
+                try
+                {
+                    datb = int.Parse(valor.Text);
+                    arbolAVL.buscar(datb);
+                    pintaR = 2;
+                    Refresh();
+                    valor.Clear();
+                }
+                catch (Exception ex)
+                {
+                    errores.SetError(valor, "Debe ser numérico");
+                }
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            errores.Clear();
+            if (valor.Text == "")
+            {
+                errores.SetError(valor, "Valor obligatorio");
+            }
+            else
+            {
+                try
+                {
+                    dato = int.Parse(valor.Text);
+                    valor.Clear();
+                    arbolAVL.Eliminar(dato);
+                    lblAltura.Text = arbolAVL.Raiz.getAltura(arbolAVL.Raiz).ToString();
+                    Refresh();
+                    Refresh();
+                    cont2++;
+                }
+                catch (Exception ex)
+                {
+                    errores.SetError(valor, "Debe ser nullérico");
+                }
+            }
+            Refresh(); Refresh(); Refresh();
+        }
+
+        private void valor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                errores.Clear();
+                if (valor.Text == "")
+                {
+                    errores.SetError(valor, "Valor obligatorio");
+                }
+                else
+                {
+                    try
+                    {
+                        dato = int.Parse(valor.Text);
+                        if (dato > 0)
+                        {
+                            arbolAVL.Insertar(dato);
+                            valor.Clear();
+                            valor.Focus();
+                            lblAltura.Text = arbolAVL.Raiz.getAltura(arbolAVL.Raiz).ToString();
+                            cont++;
+                            Refresh();
+                            Refresh();
+                        }
+                        else
+                        {
+                            errores.SetError(valor, "Debe ser un número mayor que 0");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        errores.SetError(valor, "Debe ser numérico");
+                    }
+                }
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (post.Checked)
+            {
+                arbolAVL.print = " ";
+                arbolAVL.PostOrder(arbolAVL.Raiz);
+                Orden.Text = arbolAVL.print;
+            }
+            else if (pre.Checked)
+            {
+                arbolAVL.print = " ";
+                arbolAVL.PreOrder(arbolAVL.Raiz);
+                Orden.Text = arbolAVL.print;
+            }
+            else if (ino.Checked)
+            {
+                arbolAVL.print = " ";
+                arbolAVL.InOrder(arbolAVL.Raiz);
+                Orden.Text = arbolAVL.print;
+            }
+        }
+    }
+}
+
